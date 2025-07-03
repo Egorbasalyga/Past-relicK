d50bfaaea732086b68681efe8865ce3eb2d23c28
Egor <egorbasalyga8588@gmail.com>
2025-07-03 22:52:05 +0200

    g

--- Assets/Script/Interact++/Pc.cs
+++ Assets/Script/Interact++/Pc.cs
@@ -2,14 +2,27 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Pc : InteractParent
 {
+    public Quest quest;
     public PlayerController player;
     public Canvas lio;
     public Collider box;
+    private bool open = false;
     void Start()
     {
         lio.gameObject.SetActive(false);
     }
 
+    public void off()
+    {
+        lio.gameObject.SetActive(false);
+        box.enabled = true;
+        player.Rcam = true;
+        player.nond = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        quest.NextQuest(1);
+        Destroy(this);
+    }
     public override void Interact()
     {
         lio.gameObject.SetActive(true);
@@ -18,10 +31,13 @@ public class Pc : InteractParent
       player.nond = false;
       Cursor.visible = true;
       Cursor.lockState = CursorLockMode.None;
-
+        open = true;
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.JoystickButton0)&&open)
+        {
+            off();
+        }
     }
 }
--- Assets/Script/Nazarii/ButtonClickHandler.cs
+++ Assets/Script/Nazarii/ButtonClickHandler.cs
@@ -3,24 +3,17 @@ using UnityEngine.UI;
 
 public class ButtonClickHandler : MonoBehaviour
 {
+    public Pc pc;
     public Button myButton; 
 
     void Start()
     {
-        
-        if (myButton == null)
-        {
-            Debug.LogError("Кнопка не назначена! Пожалуйста, перетащите ссылку на кнопку в инспекторе.");
-            return; 
-        }
-
-        
         myButton.onClick.AddListener(TaskOnClick);
     }
 
     
     void TaskOnClick()
     {
-        Debug.Log("Кнопка нажата!"); 
+        pc.off();
     }
 }
